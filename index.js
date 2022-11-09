d576bd5b2038537554c585381ff9f82eda681e79
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-09 17:02:16 +0600

    Basic Setup

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5500;
 
-app.get('/', (req, res))
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Kitchen Food Server Running')
+})
+
+app.listen(port, () => {
+    console.log("Our Kitchen Food Server is Running on port: ", port);
+})
