90880f57767f4620377305b980a86fe55765103a
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-09 18:46:55 +0600

    MongoDb Setup

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5500;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c8rri4v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('Kitchen Food Server Running')
