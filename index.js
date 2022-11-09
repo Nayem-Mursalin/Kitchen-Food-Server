8c2098e6d31c25f191ee1cdc6b2319f1b5a17aa7
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-09 20:37:58 +0600

    Data load from MongoDB

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const express = require('express')
-const app = express();
 const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5500;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 //middle wares
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c8rri4v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const serviceCollection = client.db('kitchenFood').collection('services');
 
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const service = await cursor.toArray();
+            res.send(service);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
 
 app.get('/', (req, res) => {
     res.send('Kitchen Food Server Running')
